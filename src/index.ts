7244c6020c0a24eebc45e5de5bfd0d59a6821d55
Vinicius Alencar <viniciusalencar176@gmail.com>
2024-06-11 21:36:08 -0300

    format webpack and create a first class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,40 @@
-console.log('OI')
+export class Empresa {
+  public readonly nome: string;
+  private readonly colaboradores: Colaborador[] = [];
+  protected readonly cnpj: string;
+
+  constructor(nome: string, cnpj: string) {
+    this.nome = nome;
+    this.cnpj = cnpj;
+  }
+
+  adicionaColaborador(colaborador: Colaborador): void {
+    this.colaboradores.push(colaborador);
+  }
+
+  mostradorColaboradores(): void {
+    for (const colaborador of this.colaboradores) {
+      console.log(colaborador);
+    }
+  }
+}
+
+export class Colaborador {
+  constructor(
+    public readonly nome: string,
+    public readonly sobrenome: string,
+  ) {}
+}
+
+const empresa1 = new Empresa('Udemy', '11.111.111/0001-11');
+const colaborador1 = new Colaborador('Vinicius', 'Alencar');
+const colaborador2 = new Colaborador('Rafael', 'Matias');
+const colaborador3 = new Colaborador('Vivian', 'Cristina');
+
+empresa1.adicionaColaborador(colaborador1);
+empresa1.adicionaColaborador(colaborador2);
+empresa1.adicionaColaborador(colaborador3);
+
+empresa1.mostradorColaboradores();
+
+// console.log(empresa1);
